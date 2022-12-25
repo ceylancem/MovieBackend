1f9c9bdaf00ea2c1af829f4bbeff99a6a1189a02
ceylancem <cemceylann@icloud.com>
2022-12-25 13:42:53 +0300

    EDIT

--- src/main/java/com/project/movie/entities/concretes/Director.java
+++ src/main/java/com/project/movie/entities/concretes/Director.java
@@ -1,19 +1,5 @@
 package com.project.movie.entities.concretes;
 
-import java.util.Date;
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 //@Data
 //@AllArgsConstructor
 //@NoArgsConstructor
